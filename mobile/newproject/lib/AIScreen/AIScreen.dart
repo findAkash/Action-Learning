8639161b0f048247b00f14e18dd45ab8b7090a31
mehmetCanishakoglu <mcanishakoglu@gmail.com>
2024-05-10 16:39:26 +0200

    initial deployment

---
+++ mobile/newproject/lib/AIScreen/AIScreen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AIScreen extends StatefulWidget {
+  @override
+  _AIScreenState createState() => _AIScreenState();
+}
+
+class _AIScreenState extends State<AIScreen> {
+  final List<String> _messages = [];
+  final TextEditingController _textController = TextEditingController();
+
+  void _sendMessage() {
+    if (_textController.text.trim().isNotEmpty) {
+      setState(() {
+        _messages.add(_textController.text);
+      });
+      _textController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true, // To keep the latest messages at the bottom
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: Alignment.topRight, // To align the chat bubble to the right
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 14.0, vertical: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.cyan, // Bubble color
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Text(
+                        _messages[index],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      labelText: 'Type a message',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8.0),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: _sendMessage,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+}
---
+++ mobile/newproject/lib/main.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:newproject/SecondRouteTeacher/SecondRouteTeacher.dart';
+import 'SecondScreen/SecondPage.dart'; // Ensure you have this path and screen correctly set up
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'School Management System',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'EPITA'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          title: Text(widget.title, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Students'),
+              Tab(text: 'Teachers'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            _buildStudentsTab(context),
+            _buildTeachersTab(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStudentsTab(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 20),
+          Center(child: Image(image: AssetImage("images/Epita.png"), height: 200, width: 200)),
+          Text('Username:', style: TextStyle(fontSize: 20)),
+          SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: BorderSide(color: Colors.grey, width: 1.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: BorderSide(color: Colors.blue, width: 2.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text('Password:', style: TextStyle(fontSize: 20)),
+          SizedBox(height: 20),
+          TextField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+              border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: BorderSide(color: Colors.grey, width: 1.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: BorderSide(color: Colors.blue, width: 2.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRouteStudent()));
+              },
+              child: Text('Sign In'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.cyan,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                padding: EdgeInsets.symmetric(horizontal: 72, vertical: 12),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTeachersTab(BuildContext context) {
+    return Container(
+      color: Colors.cyanAccent,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 20),
+            Center(child: Image(image: AssetImage("images/Epita.png"), height: 200, width: 200)),
+            Text('Username:', style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text('Password:', style: TextStyle(fontSize: 20,color: Colors.white,fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRouteTeacher()));
+                },
+                child: Text('Sign In'),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black,
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                  padding: EdgeInsets.symmetric(horizontal: 72, vertical: 12),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
